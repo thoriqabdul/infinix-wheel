d731487aab6a1b0dd65b3d511f3f6e469bc62f3a
thoriqabdul <thoriqabdul.ta@gmail.com>
2020-10-29 14:32:55 +0700

    button pres back

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -42,7 +42,7 @@
 <body style="background-color: #0D1228">
     <nav class="navbar navbar-expand-lg" style="background: #19203C; height: 70px;">
         <div class="container">
-            <a class="navbar-brand" href="#" style="color:#fff; font-size:40px">Infinix</a>
+          <a class="navbar-brand" href="{{route('landing')}}" style="color:#fff; font-size:40px">Infinix</a>
             <div class="collapse navbar-collapse" id="navbarText">
                 <span class="ml-auto" style="color:#fff; font-size:40px">
                     NOTE 8
